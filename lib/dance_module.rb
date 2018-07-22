4ddc53656b2aea57cc6c97db70d41dbb2ce2edb9
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-07-22 15:21:20 +0100

    Jo's Solution

---
+++ lib/dance_module.rb
@@ -0,0 +1,23 @@
+module Dance
+
+#twirl method
+def twirl
+  "I'm twirling!"
+end
+
+#jump method
+def jump
+  "Look how high I'm jumping!"
+end
+
+#pirouette method
+def pirouette
+   "I'm doing a pirouette"
+end
+
+#take_a_bow method
+def take_a_bow
+  "Thank you, thank you. It was a pleasure to dance for you all."
+end
+
+end
